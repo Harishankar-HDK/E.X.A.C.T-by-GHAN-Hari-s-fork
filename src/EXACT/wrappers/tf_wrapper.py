ff8a80336a813e991a5f422bb28ec06720db950c
Guren <glennmathews18@gmail.com>
2025-09-14 14:12:38 +0530

    1. Wrote initial model wrapper for pytorch 2. Started testing module for wrappers 3. Wrote example .toml template for PyPI

---
+++ src/EXACT/wrappers/tf_wrapper.py
@@ -0,0 +1,11 @@
+from base_wrapper import BaseWrapper
+
+class TFWrapper(BaseWrapper):
+    def __init__(self, model, ):
+        """Class for wrapping tensorflow models.
+           Supported Functionalities: 
+           -> 
+
+           Dependecies = ["tensorflow", "keras"]
+        """
+        pass
--- src/EXACT/wrappers/torch_wrapper.py
+++ src/EXACT/wrappers/torch_wrapper.py
@@ -1,6 +1,41 @@
-import torch
 from base_wrapper import BaseWrapper
 
 class TorchWrapper(BaseWrapper):
+    """Class for wrapping PyTorch models.
+        Supported functionalities :
+        -> .predict -> returns the logit/output returned by the model as a numpy object, moved to the cpu.
+        -> .save -> saves the model's state dictionary to specified path.
+        -> .load -> loads model weights from specified path for wrapped model.
+        -> .get_params -> returns model parameters as a Dict object).
+        -> .set_params -> sets model parameters as per input parameters of type **params
+
+        Dependencies = ["torch"]
+    """
     def __init__(self, model, optimizer = None, loss_fn = None):
-        pass
+        import torch
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+
+    def predict(self, X, **kwargs):
+        import torch
+        self.model.eval()
+        with torch.no_grad():
+            X = torch.tensor(X, dtype = torch.float32).to(self.device)
+            return self.model(X).cpu().numpy()
+    
+    def save(self, path: str):
+        import torch
+        torch.save(self.model.state_dict(), path)
+
+    def load(self, path: str):
+        import torch
+        self.model.load_state_dict(torch.load(path, map_location = self.device))
+
+    def get_params(self):
+        return {"state_dict":  self.model.state_dict}
+    
+    def set_params(self, **params):
+        self.model.load_state_dict(params["state_dict"])
