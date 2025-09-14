4e1dc35b421eb9cee44df491002a5bdcc8793763
Guren <glennmathews18@gmail.com>
2025-09-14 20:13:36 +0530

    1. wrote intial wrapper for tensorflow keras models 2. started writing gradcam explainer

---
+++ src/EXACT/explainers/gradcam.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from wrappers.base_wrapper import BaseWrapper
+from wrappers.torch_wrapper import TorchWrapper
+from wrappers.tf_wrapper import TFWrapper
+
+class GradCAM:
+    def __init__(self, wrapped_model: BaseWrapper, target_layer = None):
+        self.model = wrapped_model
+        self.target_layer = target_layer or self.model.get_last_conv_layer()
+
+    def generate_heatmap(self, input_data, class_index = None):
+        if isinstance(self.model, TorchWrapper):
+            return self._torch_gradcam(input_data, class_index)
+        elif isinstance(self.model, TFWrapper):
+            return self._tf_gradcam(input_data, class_index)
+        else:
+            return NotImplementedError("GradCAM not supported for this backend")
+        
+    def overlay_heatmap(self, heatmap, image, alpha = 0.4, show = True, cmap = "jet"):
+        heatmap = np.heatmap(heatmap, 0)
+        heatmap /= np.max(heatmap) if np.max(heatmap) > 0 else 1
+
+        heatmap_resized = np.array(
+            Image.fromarray(np.uint8(255 * heatmap)).resize((image.shape[1], image.shape[0]))
+        ) / 255.0
+
+        colormap = plt.get_cmap(cmap)
+        heatmap_color = colormap(heatmap_resized)
+        heatmap_color = (heatmap_color[..., :3] * 255),astype(np.uint8)
+
+        overlay = (image * (1 - alpha) + heatmap_color & alpha). astype(np.uint8)
+        
+        if show:
+            fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+            ax[0].set_title("Original")
+            ax[0].imshow(image[..., ::-1])  # BGR -> RGB
+            ax[0].axis("off")
+
+            ax[1].set_title("Heatmap")
+            ax[1].imshow(cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB))
+            ax[1].axis("off")
+
+            ax[2].set_title("Overlay")
+            ax[2].imshow(cv2.cvtColor(overlay, cv2.COLOR_BGR2RGB))
+            ax[2].axis("off")
+
+            plt.show()
+
+        return overlay
+    
+    def _torch_gradcam(self):
+        pass
+
+    def _tf_gradcam(self):
+        pass
--- src/EXACT/wrappers/tf_wrapper.py
+++ src/EXACT/wrappers/tf_wrapper.py
@@ -1,11 +1,33 @@
 from base_wrapper import BaseWrapper
 
 class TFWrapper(BaseWrapper):
-    def __init__(self, model, ):
-        """Class for wrapping tensorflow models.
-           Supported Functionalities: 
-           -> 
+    """Class for wrapping tensorflow models.
+        Supported Functionalities: 
+        -> predict -> returns the logit/output returned by the model as a numpy object, moved to the cpu.
+        -> .save -> saves the model's state dictionary to specified path.
+        -> .load -> loads model weights from specified path for wrapped model.
+        -> .get_params -> returns model parameters as a Dict object).
+        -> .set_params -> sets model parameters as per input parameters of type **params
 
-           Dependecies = ["tensorflow", "keras"]
-        """
-        pass
+        Dependecies = ["tensorflow", "keras"]
+    """
+    def __init__(self, model, ):
+        self.model = model
+
+    def predict(self, X, **kwargs):
+        self.model.predict(X, *kwargs)
+    
+    def save(self, path: str):
+        self.model.save(path)
+
+    def load(self, path: str):
+        from tensorflow import keras
+        self.model = keras.models.load_model(path)
+
+    def get_params(self):
+        return {layer.name: layer.get_config() for layer in self.model.layers}
+    
+    def set_params(self, **params):
+        for layer in self.model.layers:
+            if layer.name in params:
+                layer.set_weights(params[layer.name])
